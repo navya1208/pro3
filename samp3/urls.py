6993f80962031028ebd85778b5be5f15a6383218
unknown <navyadigajerla96@gmail.com>
2020-07-21 19:11:40 +0530

    second commit

--- samp3/urls.py
+++ samp3/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('sample1/',views.sample1,name="sample1"),
     path('sample2/',views.sample2,name="sample2"),
+    path('sample3/',views.sample3,name="sample3"),
+    path('sample4/',views.sample4,name="sample4"),
+
 ]
--- samp3/views.py
+++ samp3/views.py
@@ -8,4 +8,19 @@ def sample1(request):
   return render(request,"sample1.html")
 
 def sample2(request):
-  return render(request,"directory\sample2.html")
+  return render(request,"directory\sample2.html", context={'name':'Navya','course':'Python fullstack'})
+
+def sample3(request):
+  fruits=['banana','apple','orange','grapes','guava','watermelon']
+  return render(request,"directory\sample3.html", context={'fruits':fruits})
+
+def sample4(request):
+  a=45;
+  b=35;
+  sum=a+b
+  sub=a-b
+  mul=a*b
+  div=a//b
+
+  # return render(request,"directory\sample4.html", context={'a':30,'b':45})
+  return render(request,"directory\sample4.html",context={'a':a,'b':b,'sum':sum,'sub':sub,'mul':mul,'div':div})
